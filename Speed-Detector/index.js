cd2de743ef1a7b3617841ef979133d8c37b9b89c
Edpaul1 <saidarajab101@gmail.com>
2023-03-18 15:23:33 +0300

    Speed Detector

---
+++ Speed-Detector/index.js
@@ -0,0 +1,17 @@
+function speedDetector() {
+    let speed = prompt('Enter your Speed: ');
+  const speedLimit = 70;
+  const kmPerDemerit = 5;
+
+  if (speed <= speedLimit) {
+    console.log("Ok");
+  } else {
+    const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemerit);
+    if (demeritPoints > 12) {
+      console.log("License suspended");
+    } else {
+      console.log("Points: " + demeritPoints);
+    }
+  }
+}
+speedDetector();
