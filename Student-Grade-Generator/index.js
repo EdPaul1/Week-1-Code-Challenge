72750d421cad27c11ecf9f5e5a6168cef524d41d
Edpaul1 <saidarajab101@gmail.com>
2023-03-18 15:07:56 +0300

    Student Grade Generator

---
+++ Student-Grade-Generator/index.js
@@ -0,0 +1,21 @@
+const grader = function(){
+    let grade = prompt('Enter your Grade: ');
+    let value;
+    if(grade >= 0 && grade <= 100){
+        console.log('Your input is ' + grade);
+        if (grade > 79){
+            console.log('Your grade is A');
+        }else if(grade > 60 && grade <= 79){
+            console.log('Your grade is B');
+        } else if(grade > 49 && grade <= 59){
+            console.log('Your grade is C');
+        }else if (grade >= 40 && grade <= 49){
+            console.log('Your grade is D');
+        }else if (grade < 40){
+            console.log('Your grade is E');
+        };
+    }else{
+        console.log('Incorrect value');
+    }
+}
+grader();
