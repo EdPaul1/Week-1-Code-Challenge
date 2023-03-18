245fe9e110014dd7d60660a0405280850e33fe22
Edpaul1 <saidarajab101@gmail.com>
2023-03-18 18:10:32 +0300

    Speed Detector

--- Speed-Detector/index.js
+++ Speed-Detector/index.js
@@ -1,11 +1,13 @@
 function speedDetector() {
-    let speed = prompt('Enter your Speed: ');
+    let speed = prompt('Enter your Speed: ');  // function prompts the user to input the speed
   const speedLimit = 70;
+  // A point is given for speed above the speedlimit. Every 5km above earns a point.
   const kmPerDemerit = 5;
 
   if (speed <= speedLimit) {
     console.log("Ok");
   } else {
+    // math.floor ensures only whole numbers are returned. Math.floor rounds down
     const demeritPoints = Math.floor((speed - speedLimit) / kmPerDemerit);
     if (demeritPoints > 12) {
       console.log("License suspended");
